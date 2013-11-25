fe521b104e2f98ad46ffc6a55814d4e760d868c1
Rich Hildred <rhildred@gmail.com>
2013-11-24 21:14:00 -0500

    first crack at starting point for class project

--- php/projectStart/categories/index.php
+++ php/projectStart/categories/index.php
@@ -6,23 +6,53 @@ $db = new PDO ( "mysql:host=localhost;dbname=swag", "root", "" );
 $aUrls = explode('/', $_SERVER['REQUEST_URI']);
 $nId =  array_pop($aUrls);
 
-if (is_numeric ( $nId )) {
-	// get a single cd
-	$stmt = $db->prepare("SELECT * FROM categories WHERE id = ?");
-	$stmt->execute(array($nId));
-	$result = $stmt->fetchObject();
-	echo json_encode($result);
-} else {
-	
-	// prepare with 3 placeholders
-	$stmt = $db->prepare ( "SELECT * FROM categories" );
-	
-	// execute with 3 parameters
-	$stmt->execute ();
-	
-	$results = $stmt->fetchAll ( PDO::FETCH_OBJ );
-	
-	echo json_encode ( $results );
+$sTable = array_pop($aUrls);
+$sSQL = "SELECT * FROM ";
+$sWhere = "";
+$aBinds = array();
+switch($sTable){
+	case 'categories':
+		$sSQL .= 'categories';
+		if(is_numeric($nId)){
+			$sWhere .= 'id = ?';
+			array_push($aBinds, $nId);
+		}
+		break;
+	case 'products':
+		$sSQL = 'SELECT a.*, b.url FROM products AS a, images AS b';
+		$sWhere .= 'a.id = b.item_id AND b.sequence_id = 1 AND category_id = ?';
+		array_push($aBinds, array_pop($aUrls));
+		if(is_numeric($nId)){
+			$sWhere .= ' AND a.id = ?';
+			array_push($aBinds, $nId);
+		}
+		break;
+	case 'images':
+		$sSQL .= 'images';
+		$sWhere .= 'item_id = ?';
+		array_push($aBinds, array_pop($aUrls));
+		if(is_numeric($nId)){
+			$sWhere .= ' AND sequence_id = ?';
+			array_push($aBinds, $nId);
+		}
+		break;
+	default:
+		throw(new Exception("bad path " . $sTable));
 }
+if(is_numeric($nId)){
+	// get a single item
+	$stmt = $db->prepare($sSQL . " WHERE ". $sWhere);
+	$stmt->execute($aBinds);
+	$result = $stmt->fetchObject();
+} else {
+	// get all of the items
+	if($sWhere != ""){
+		$sSQL .= " WHERE " . $sWhere;
+	}
+	$stmt = $db->prepare($sSQL);
+	$stmt->execute($aBinds);
+	$result = $stmt->fetchAll ( PDO::FETCH_OBJ );
+}
+echo json_encode($result);
 
 ?>
